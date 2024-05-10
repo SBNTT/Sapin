6b775b4beb01440b42ab25e93f81043d63112456
SBNTT <bysebastien@gmail.com>
2024-05-10 11:57:24 +0200

    simplifications

--- examples/todos-vanilla/src/Component/TaskListItem.php
+++ examples/todos-vanilla/src/Component/TaskListItem.php
@@ -7,26 +7,21 @@ use App\Model\TaskState;
 
 final readonly class TaskListItem
 {
+    public int $inProgressState;
+    public int $doneState;
+
     public function __construct(
         public Task $task,
     )
     {
-    }
-
-    public function getInProgressState(): TaskState
-    {
-        return TaskState::IN_PROGRESS;
-    }
-
-    public function getDoneState(): TaskState
-    {
-        return TaskState::DONE;
+        $this->inProgressState = TaskState::IN_PROGRESS->value;
+        $this->doneState = TaskState::DONE->value;
     }
 } ?>
 
 <template>
     <li
-            :id="'task-' . $this->task->id . '-list-item'"
+            id="task-{{ $this->task->id }}-list-item"
             class="group flex items-center p-4 bg-gray-50 hover:bg-green-50 border rounded-xl hover:shadow-lg transition"
     >
         <div class="flex-1">
@@ -41,7 +36,7 @@ final readonly class TaskListItem
         <div class="flex items-center gap-3 mr-2 opacity-0 group-hover:opacity-100 transition">
             <a
                     :if="$this->task->isPending()"
-                    href="/update-task-state?id={{ $this->task->id }}&state={{ $this->getInProgressState()->value }}"
+                    href="/update-task-state?id={{ $this->task->id }}&state={{ $this->inProgressState }}"
                     class="py-2 px-4 rounded-full bg-blue-50 text-blue-400 border border-blue-300 border-opacity-0 hover:border-opacity-100 transition"
             >
                 Start
@@ -49,7 +44,7 @@ final readonly class TaskListItem
 
             <a
                     :else-if="$this->task->isInProgress()"
-                    href="/update-task-state?id={{ $this->task->id }}&state={{ $this->getDoneState()->value }}"
+                    href="/update-task-state?id={{ $this->task->id }}&state={{ $this->doneState }}"
                     class="py-2 px-4 rounded-full bg-green-50 text-green-500 border border-green-400 border-opacity-0 hover:border-opacity-100 transition"
             >
                 Done
